553ac82db3bf4441688503ae0346a21c966f9780
Diego Barboza <diegobarboza.medeiros@gmail.com>
2023-12-15 15:45:07 -0300

    Fundamentos React

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <>
+//     <h1 id="title">
+//       <span>Hello world</span>
+//     </h1>
+//   </>
+// );
+ReactDOM.render(
+ <h1 id="title">
+   <span>Hello world!</span>
+ </h1>,
+document.getElementById('root'),
+);
